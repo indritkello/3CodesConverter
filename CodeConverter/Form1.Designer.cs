ee3775d3d8effa71b0e74b0ac9902e8077f1573d
indritkello <indritkello97@gmail.com>
2018-11-28 12:53:45 +0100

    initial commit for 3 codes converter

---
+++ CodeConverter/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿using System;
+
+namespace CodeConverter
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.comboBox1 = new System.Windows.Forms.ComboBox();
+      this.comboBox2 = new System.Windows.Forms.ComboBox();
+      this.comboBox3 = new System.Windows.Forms.ComboBox();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(45, 48);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(28, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Kodi";
+      this.label1.Click += new System.EventHandler(this.label1_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(170, 45);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(100, 20);
+      this.textBox1.TabIndex = 1;
+      this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(48, 146);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(100, 29);
+      this.button1.TabIndex = 5;
+      this.button1.Text = "Konverto";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(170, 178);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(100, 20);
+      this.textBox2.TabIndex = 6;
+      this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+      this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(167, 162);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(48, 13);
+      this.label2.TabIndex = 7;
+      this.label2.Text = "Rezultati";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(266, 146);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(0, 13);
+      this.label3.TabIndex = 8;
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(303, 52);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(0, 13);
+      this.label4.TabIndex = 9;
+      this.label4.Click += new System.EventHandler(this.label4_Click);
+      // 
+      // comboBox1
+      // 
+      this.comboBox1.DataSource = new CodeConverter.Form1.HapDekodimi[] {
+        CodeConverter.Form1.HapDekodimi.KodiGray,
+        CodeConverter.Form1.HapDekodimi.KodiPlus3,
+        CodeConverter.Form1.HapDekodimi.KodiHamming};
+
+      this.comboBox1.Location = new System.Drawing.Point(48, 93);
+      this.comboBox1.Name = "comboBox1";
+      this.comboBox1.Size = new System.Drawing.Size(109, 21);
+      this.comboBox1.TabIndex = 10;
+      // 
+      // comboBox2
+      // 
+      this.comboBox2.DataSource = new CodeConverter.Form1.HapDekodimi[] {
+        CodeConverter.Form1.HapDekodimi.KodiGray,
+        CodeConverter.Form1.HapDekodimi.KodiPlus3,
+        CodeConverter.Form1.HapDekodimi.KodiHamming};
+    
+      this.comboBox2.Location = new System.Drawing.Point(170, 93);
+      this.comboBox2.Name = "comboBox2";
+      this.comboBox2.Size = new System.Drawing.Size(121, 21);
+      this.comboBox2.TabIndex = 11;
+      // 
+      // comboBox3
+      // 
+      this.comboBox3.DataSource = new CodeConverter.Form1.HapDekodimi[] {
+        CodeConverter.Form1.HapDekodimi.KodiGray,
+        CodeConverter.Form1.HapDekodimi.KodiPlus3,
+        CodeConverter.Form1.HapDekodimi.KodiHamming};
+
+      this.comboBox3.Location = new System.Drawing.Point(306, 93);
+      this.comboBox3.Name = "comboBox3";
+      this.comboBox3.Size = new System.Drawing.Size(121, 21);
+      this.comboBox3.TabIndex = 12;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(535, 261);
+      this.Controls.Add(this.comboBox3);
+      this.Controls.Add(this.comboBox2);
+      this.Controls.Add(this.comboBox1);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.ComboBox comboBox1;
+    private System.Windows.Forms.ComboBox comboBox2;
+    private System.Windows.Forms.ComboBox comboBox3;
+  }
+}
+
---
+++ CodeConverter/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeConverter
+{
+  public partial class Form1 : Form
+  {
+
+    public enum HapDekodimi
+    {
+     KodiGray,
+     KodiPlus3,
+     KodiHamming
+
+    };
+    public List<HapDekodimi> hapat{ get; set; }
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+      int grayToBinary(int num)
+    {
+      int temp = num ^ (num >> 8);
+      temp ^= (temp >> 4);
+      temp ^= (temp >> 2);
+      temp ^= (temp >> 1);
+      return temp;
+    }
+    int binaryToGray(int num)
+    {
+      return (num >> 1) ^ num;
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      //Use this if input is Integer
+      //string binary = Convert.ToString(Convert.ToInt32(textBox1.Text),toBase: 2);
+      hapat = new List<HapDekodimi>();
+      string binary = textBox1.Text;
+      int binaryInt = 0;
+      try
+      {
+        binaryInt = Convert.ToInt32(binary,2);
+      }
+      catch (Exception)
+      {
+        label4.Text = "Nuk keni shkruar nje numer binar! Provojeni serisht!";
+      }
+      hapat.Add((HapDekodimi)Enum.Parse(typeof(HapDekodimi), comboBox3.Text));
+      hapat.Add((HapDekodimi)Enum.Parse(typeof(HapDekodimi), comboBox2.Text));
+      hapat.Add((HapDekodimi)Enum.Parse(typeof(HapDekodimi), comboBox1.Text));
+      if (hapat.Distinct().Count() < 3)
+      {
+        label4.Text += "3 vlerat e zgjedhura duhet te jene te ndryshme!";
+        return;
+      }
+      textBox2.Text = DekodoVlerat(binary, hapat);
+    }
+    public string DekodoKodinGray(string binary)
+    {
+      try
+      {
+        int num = Convert.ToInt32(binary,2);
+        int temp = num ^ (num >> 8);
+        temp ^= (temp >> 4);
+        temp ^= (temp >> 2);
+        temp ^= (temp >> 1);
+
+        return Convert.ToString(temp, 2);
+      }
+      catch(System.FormatException ex)
+      {
+        label4.Text += ex.Message;
+      }
+      
+        return "";
+      
+    }
+    public string DekodoKodin3Plus(string binary)
+    {
+      int num = Convert.ToInt32(binary,2);
+      int temp = num ^ (num >> 8);
+      temp ^= (temp >> 4);
+      temp ^= (temp >> 2);
+      temp ^= (temp >> 1);
+      return Convert.ToString(temp, 2);
+    }
+    public string DekodoVlerat(string binary, List<HapDekodimi> hapatEDekodimit)
+    {
+      if (hapatEDekodimit.Count() == 0)
+      {
+        return binary;
+      }
+      switch (hapatEDekodimit[0])
+      {  case HapDekodimi.KodiGray:
+              binary = DekodoKodinGray(binary);
+          break;
+        case HapDekodimi.KodiPlus3:
+          binary = DekodoKodin3Plus(binary);
+          break;
+        case HapDekodimi.KodiHamming:
+          
+          break;
+
+      }
+      hapatEDekodimit.RemoveAt(0);
+      binary = DekodoVlerat(binary, hapatEDekodimit);
+      return binary;
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label4_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void textBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
